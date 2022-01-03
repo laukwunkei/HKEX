27e08c5725acf0b3dc803f95889d3539a4fbc345
LouisLau <louislau@LouisLaus-MacBook-Pro.local>
2022-01-03 22:19:30 +1100

    make it better

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 # Frontend and web framework
 from flask import Flask, render_template, request, session, redirect, flash, send_file
-from flask_bootstrap import Bootstrap 
+from flask.helpers import total_seconds
+from flask_bootstrap import Bootstrap
+import os, json
+from werkzeug.utils import secure_filename
 
-
+# Flask configuration
 app = Flask(__name__)
 Bootstrap(app)
 app.secret_key = 'dljsaklqk24e21cjn!Ew@@dsa5'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+# S3 config
+AWS_BUCKET_NAME = 'cheekybucket'
+AWS_ACCESS_KEY = 'AKIA2G3PNNUOCXY7HX72' 
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_DOMAIN = 'http://cheekybucket.s3.amazonaws.com/'
+
 # Backend
-import os
 from os import error
 from random import randrange
 from pandas.core.base import NoNewAttributesMixin
@@ -22,7 +30,38 @@ import re
 import akshare as ak
 import matplotlib.pyplot as plt
 import matplotlib
+import locale
 matplotlib.use('Agg')
+
+import boto3, botocore
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
+)
+
+def upload_file_to_s3(file, acl="public-read"):
+    filename = secure_filename(file.filename)
+    try:
+        s3.upload_fileobj(
+            file,
+            os.getenv("AWS_BUCKET_NAME"),
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+
+    except Exception as e:
+        # This is a catch all exception, edit this part to fit your needs.
+        print("Something Happened: ", e)
+        return e
+    
+
+    # after upload file to s3 bucket, return filename of the uploaded file
+    return file.filename
 
 '''
 Web scraper for repurchase data
@@ -58,9 +97,9 @@ def get_repurchase_raw_data(day):
             end = str(date).split()[0].replace('-','')
             main_url = "https://www.hkexnews.hk/reports/sharerepur/documents/SRRPT" + end + '.xls'
             resp=requests.get(main_url)
-            output = open("repurchase_data/{}.xls".format(end), 'wb')
+            output = open("tmp/{}.xls".format(end), 'wb')
             output.write(resp.content)
-            temp_df = pd.read_excel("repurchase_data/{}.xls".format(end))
+            temp_df = pd.read_excel("tmp/{}.xls".format(end))
 
             # Get the index that matches my condition
             temp_df = temp_df.loc[temp_df['Unnamed: 1'] < '9999999']
@@ -86,7 +125,6 @@ def single_name_information(ticker, df, day):
     Extract repurchase information for a single stock
     including total repurchase stock, average repurchase price and percentage to common shares
     '''
-    print(df)
     single_name_df = pd.DataFrame()
     single_name_df = df.loc[df['Code'] == str(ticker)]
     if single_name_df.empty:
@@ -99,10 +137,13 @@ def single_name_information(ticker, df, day):
         Currency = row['Total_paid'].split(" ")[0]
         Total_paid = Total_paid + int(row['Total_paid'].split(" ")[1].split(".")[0].replace(',',''))
         row['Date'] = row['Date'].split()[0].replace('/','-')
+    
+    common_share = float(single_name_df.tail(1)["Number_of_sec_purchased"].iloc[0].replace(',',''))/(float(single_name_df.tail(1)["Perc_number_of_shares_issue"].iloc[0])*0.01)
+    print(common_share)
 
     # Get latest repurchase info
-    summary = "在{}天内，{}一共回购了{}股，平均回购股价为{} - 共{}元 。截止上一次股东决议会，一共回购了{}股，占流通股百分之{}%".format(day, single_name_df['Stock'].iloc[0], 
-    Total_shares, round(Total_paid/Total_shares,2), Total_paid, single_name_df.tail(1)["Number_of_sec_purchased"].iloc[0], single_name_df.tail(1)["Perc_number_of_shares_issue"].iloc[0])
+    summary = "在{}天内，{}一共回购了{}股，平均回购股价为{} - 共{}元, 占流通股{}%".format(day, single_name_df['Stock'].iloc[0], f"{Total_shares:,}"
+    , round(Total_paid/Total_shares,2), f"{Total_paid:,}", round(Total_shares/common_share,5))
     return single_name_df, summary
     
 
@@ -136,6 +177,7 @@ def get_repurchase_and_price(repurchase_data, share_data, dates):
         date_num_stock_dict[row['Date']] = row['Num_sec']
 
     # Plot price vs repurchase data
+    
     plt.figure(2)
     fig, axs = plt.subplots(2)
     axs[0].bar(*zip(*sorted(date_num_stock_dict.items())), alpha = 0.2)
@@ -149,6 +191,7 @@ def get_repurchase_and_price(repurchase_data, share_data, dates):
     plt.setp(axs[0].get_xticklabels(), visible=False)
     plt.savefig('static/'+str(random) + 'price_repurchase_history.png', dpi=80)
     return 'static/'+str(random) + 'price_repurchase_history.png'
+    
 
 '''
 Pre-process industry data
@@ -172,7 +215,6 @@ def get_stock_industry_summary(ticker, industry_data, raw_repurchase_data):
     repurchase_df['Code'] = repurchase_df['Code'].astype(float, errors = 'raise')
     industry_df['sum'] = 0
     new_df = industry_data[industry_data['Ticker'].isin(raw_repurchase_data['Code'])]
-    
     # Transform total paid string
     paid_list = []
     for index, row in repurchase_df.iterrows():
@@ -191,6 +233,7 @@ def get_stock_industry_summary(ticker, industry_data, raw_repurchase_data):
     
     # Get sector summary
     sector = new_df.loc[new_df['Ticker'] == ticker]['Sector'].iloc[0]
+    num_names = len(industry_df.loc[industry_df['Sector'] == sector]['Stock'])
     Name = new_df.loc[new_df['Ticker'] == ticker]['Stock'].iloc[0]
     sector_df = new_df.loc[new_df['Sector'] == sector]
     num_repurchase, sector_average = len(sector_df.index), sector_df['Total_paid'].mean()
@@ -206,7 +249,7 @@ def get_stock_industry_summary(ticker, industry_data, raw_repurchase_data):
     plt.tight_layout()
     random = randrange(1000) 
     plt.savefig('static/'+str(random) + 'stock_sector_summary.png')
-    return new_df, num_repurchase, sector_average, Name, sector, 'static/'+str(random) + 'stock_sector_summary.png'
+    return new_df, num_repurchase, sector_average, Name, sector, 'static/'+str(random) + 'stock_sector_summary.png', num_names
 
 '''
 Create heatmap for industry comparison
@@ -222,7 +265,7 @@ def get_entire_industry_summary(df):
         temp_sum = df.loc[df['Sector'] == row['Sector'], 'Total_paid'].sum()
         sector_sum.append(temp_sum)
     sector_summary_df['sum'] = sector_sum
-    sector_summary_df = sector_summary_df.sort_values(by ='sum', ascending=False)
+    sector_summary_df = sector_summary_df.sort_values(by ='sum', ascending=False).head(10)
 
     # Create chart
     fig, ax1 = plt.subplots(1, 1, sharey=True)
@@ -247,8 +290,8 @@ def restart():
     for filename in os.listdir("static"):
         if filename.endswith('png'):
             os.remove("static/"+filename)
-    for filename in os.listdir("repurchase_data"):
-        os.remove("repurchase_data/"+filename)
+    for filename in os.listdir("tmp"):
+        os.remove("tmp/"+filename)
     
     return render_template('index.html')
 
@@ -266,12 +309,12 @@ def summary():
         share_data = get_share_data(int(ticker), dates)
         photo1 = get_repurchase_and_price(stock_repurchase_data, share_data, dates)
         raw_industry = get_industry_data()
-        industry_df, num_repurchase, sector_average, stock_name, sector, photo2 = get_stock_industry_summary(int(ticker), raw_industry, raw_df)
+        industry_df, num_repurchase, sector_average, stock_name, sector, photo2, num_names = get_stock_industry_summary(int(ticker), raw_industry, raw_df)
+
         photo3, highest_industry = get_entire_industry_summary(industry_df)
         return render_template('summary.html', stock_repurchase_data = stock_repurchase_data.to_html(), summary=summary, 
-            num_repurchase = num_repurchase, sector_average = sector_average, stock_name = stock_name, sector = sector, photo1 = photo1,
-            photo2 = photo2, photo3 = photo3, highest_industry = highest_industry)
-
+            num_repurchase = num_repurchase, sector_average = f"{int(sector_average):,}", stock_name = stock_name, sector = sector, photo1 = photo1,
+            photo2 = photo2, photo3 = photo3, highest_industry = highest_industry, num_names = num_names)
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
